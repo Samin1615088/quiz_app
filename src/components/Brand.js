9a72abc256e3fd0ec7fafa474628de85fd76b4b0
Samin1615088 <samin.shafkat@gmail.com>
2023-05-03 14:29:12 +0600

    added brand, account, Nav component

---
+++ src/components/Brand.js
@@ -0,0 +1,12 @@
+export default function Brand() {
+    return (
+        <ul>
+            <li>
+                <a href="index.html" className="brand">
+                    <img src="./images/logo-bg.png" alt="Learn with Sumit Logo" />
+                    <h3>Learn with Sumit</h3>
+                </a>
+            </li>
+        </ul>
+    );
+}
