3d13f598a40546830515958ca5cd29c845206d3c
Samin1615088 <samin.shafkat@gmail.com>
2023-05-05 17:54:43 +0600

    l-5 completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Layout from "./components/Layout";
-import Home from "./components/pages/Home";
+// import Home from "./components/pages/Home";
+// import Signup from "./components/pages/Signup";
+import Login from "./components/pages/Login";
 import "./styles/App.css";
+
 function App() {
   return (
     <Layout>
-      <Home />
+      {/* <Home /> */}
+      {/* <Signup /> */}
+      <Login />
+
     </Layout>
   );
 }
