f3e7aee16b8d7bb738fa64dcfb64e68a0a202694
Samin1615088 <samin.shafkat@gmail.com>
2023-05-06 05:37:51 +0600

    <a/> link issue solved

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import classes from "../styles/Button.module.css";
 
 export default function Button({ className, children }) {
     return (
+        // If I change this div to Button. My nav button does not work
         <div className={`${classes.button} ${className}`}>
             {children}
         </div>
