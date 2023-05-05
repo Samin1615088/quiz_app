59b4628942f1c63e194164ece9ae0b999ed9cba6
Samin1615088 <samin.shafkat@gmail.com>
2023-05-06 02:14:22 +0600

    half done with some issue

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Login from "./components/pages/Login";
 import Quiz from "./components/pages/Quiz";
 import Result from "./components/pages/Result";
 import Signup from "./components/pages/Signup";
+import { AuthProvider } from "./contexts/AuthContext";
 import "./styles/App.css";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes >
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/quiz" element={<Quiz />} />
-          <Route exact path="/result" element={<Result />} />
-        </Routes>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Routes >
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/quiz" element={<Quiz />} />
+            <Route exact path="/result" element={<Result />} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 }
