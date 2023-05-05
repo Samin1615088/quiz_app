2066678cf575446b842761c74e507c55894d7e0f
Samin1615088 <samin.shafkat@gmail.com>
2023-05-06 02:33:59 +0600

    issue solved temporary

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,20 @@
-import { initializeApp } from "firebase/app";
-
-// firebase configuration
-const app = initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENTID,
-});
+// const app = initializeApp({
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+//     measurementId: process.env.REACT_APP_MEASUREMENTID,
+// });
+const app = {
+    apiKey: "AIzaSyCZ4TfJAkqIET14ctQZDMEqmGJ4fzIbIVk",
+    authDomain: "react-quiz-dev-cbp.firebaseapp.com",
+    projectId: "react-quiz-dev-cbp",
+    storageBucket: "react-quiz-dev-cbp.appspot.com",
+    messagingSenderId: "198586883525",
+    appId: "1:198586883525:web:e4dd705b0bfc724305bfac",
+    measurementId: "G-37NDYJ4ML9"
+};
 
 export default app;
